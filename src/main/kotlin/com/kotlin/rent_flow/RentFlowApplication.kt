6cc56dbf6fd87770943c123b496f69c66ceba2b7
Mirudull-D <d.mirudull@gmail.com>
2026-05-26 10:49:30 +0530

    change the config files to local DB

--- src/main/kotlin/com/kotlin/rent_flow/RentFlowApplication.kt
+++ src/main/kotlin/com/kotlin/rent_flow/RentFlowApplication.kt
@@ -2,11 +2,13 @@ package com.kotlin.rent_flow
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.TimeZone
 
 @SpringBootApplication
 class RentFlowApplication
 
 fun main(args: Array<String>) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"))
     runApplication<RentFlowApplication>(*args)
 }
 
